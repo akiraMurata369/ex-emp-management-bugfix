add94b147379022e027532cbde76e9f6b6257cff
axz369 <gp21a169@oecu.jp>
2025-06-09 14:41:21 +0900

    [delete] 不要なメソッド

--- src/main/java/com/example/controller/LogController.java
+++ src/main/java/com/example/controller/LogController.java
@@ -12,24 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LogController {
 
 	/**
-	 * システム内で例外発生を行うメソッド.
-	 * ここで発生した例外はGlobalExceptionHandlerが捕獲し処理をします
-	 * 
-	 * @throws ArithmeticException このメソッドは必ずArithmeticExceptionを発生します
-	 */
-	@GetMapping("/exception")
-	public String throwsException() {
-		// 0で除算、非検査例外であるArithmeticExceptionが発生！
-		System.out.println("例外発生前");
-		System.out.println(10 / 0); // ←このタイミングでGlobalExceptionHandlerに処理が飛ぶ
-		System.out.println("例外発生後");
-
-		return "通常はここにHTML名を書くが、ここまで処理は来ない";
-	}
-
-
-
-	/**
 	 *  ログイン画面に遷移する.
 	 *
 	 * @return ログイン画面
