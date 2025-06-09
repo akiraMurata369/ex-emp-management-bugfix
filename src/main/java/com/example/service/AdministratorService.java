ccfdafa92b074477beab65209486dfffa4e5f818
axz369 <gp21a169@oecu.jp>
2025-06-09 13:26:42 +0900

    5-3の機能追加

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,7 +10,7 @@ import com.example.repository.AdministratorRepository;
 
 /**
  * 管理者情報を操作するサービス.
- * 
+ *
  * @author igamasayuki
  *
  */
@@ -20,12 +21,19 @@ public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	/**
 	 * 管理者情報を登録します.
 	 * 
 	 * @param administrator 管理者情報
 	 */
 	public void insert(Administrator administrator) {
+		// パスワードをハッシュ化して保存
+		String encodedPassword = passwordEncoder.encode(administrator.getPassword());
+		administrator.setPassword(encodedPassword);
+
 		administratorRepository.insert(administrator);
 	}
 
@@ -37,7 +45,18 @@ public class AdministratorService {
 	 * @return 管理者情報 存在しない場合はnullが返ります
 	 */
 	public Administrator login(String mailAddress, String password) {
-		Administrator administrator = administratorRepository.findByMailAddressAndPassward(mailAddress, password);
+		Administrator administrator = administratorRepository.findByMailAddress(mailAddress);
+
+		// 管理者が存在しない場合はnullを返す
+		if (administrator == null) {
+			return null;
+		}
+
+		// パスワードが一致しない場合はnullを返す
+		if (!(passwordEncoder.matches(password, administrator.getPassword()))) {
+			return null;
+		}
+
 		return administrator;
 	}
 }
